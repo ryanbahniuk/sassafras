a3ce2269753803e748c57b334cffb0d4a2063419
rtrufin <7871937+rtrufin@users.noreply.github.com>
2017-10-25 09:49:41 +0300

    Fixed indentation

--- src/sassaby.js
+++ src/sassaby.js
@@ -8,64 +8,64 @@ var parsers = require('./parsers');
 var normalize = require('normalize-path');
 
 function setVariables(varz) {
-    var sassVariables = '';
-    for (var variableName in varz) {
-        sassVariables = sassVariables + '$' + variableName + ':' + varz[variableName] + ';';
-    }
-    return sassVariables;
+  var sassVariables = '';
+  for (var variableName in varz) {
+    sassVariables = sassVariables + '$' + variableName + ':' + varz[variableName] + ';';
+  }
+  return sassVariables;
 }
 
 function setDependencies(dependencies) {
-    var sassImports = '';
-    dependencies.forEach(function(fileName) {
-        sassImports = sassImports + "@import '" + normalize(fileName) + "';";
-    });
-    return sassImports;
+  var sassImports = '';
+  dependencies.forEach(function(fileName) {
+    sassImports = sassImports + "@import '" + normalize(fileName) + "';";
+  });
+  return sassImports;
 }
 
 function Sassaby(path, options) {
-    options = options || {};
-    this.path = path;
-    this.file = fs.readFileSync(normalize(path), 'utf-8').toString();
-    this.variables = '';
-    this.dependencies = '';
+  options = options || {};
+  this.path = path;
+  this.file = fs.readFileSync(normalize(path), 'utf-8').toString();
+  this.variables = '';
+  this.dependencies = '';
 
-    if (options.variables) {
-        this.variables = setVariables(options.variables);
-    }
+  if (options.variables) {
+    this.variables = setVariables(options.variables);
+  }
 
-    if (options.dependencies) {
-        this.dependencies = setDependencies(options.dependencies);
-    }
+  if (options.dependencies) {
+    this.dependencies = setDependencies(options.dependencies);
+  }
 }
 
 Sassaby.prototype = {
-    imports: function(name) {
-        var message = 'Could not find an import statement with ' + name + ' in file.';
-        assert(parsers.hasImport(this.file, name), message);
-    },
+  imports: function(name) {
+    var message = 'Could not find an import statement with ' + name + ' in file.';
+    assert(parsers.hasImport(this.file, name), message);
+  },
 
-    doesNotImport: function(name) {
-        var message = 'Found an import statement with ' + name + ' in file.';
-        assert(!parsers.hasImport(this.file, name), message);
-    },
+  doesNotImport: function(name) {
+    var message = 'Found an import statement with ' + name + ' in file.';
+    assert(!parsers.hasImport(this.file, name), message);
+  },
 
-    includedMixin: function(call) {
-        return new Mixin('included', this.variables, this.dependencies, this.file, call);
-    },
+  includedMixin: function(call) {
+    return new Mixin('included', this.variables, this.dependencies, this.file, call);
+  },
 
-    standaloneMixin: function(call) {
-        return new Mixin('standalone', this.variables, this.dependencies, this.file, call);
-    },
+  standaloneMixin: function(call) {
+    return new Mixin('standalone', this.variables, this.dependencies, this.file, call);
+  },
 
-    func: function(call) {
-        return new Func(this.variables, this.dependencies, this.file, call);
-    }
+  func: function(call) {
+    return new Func(this.variables, this.dependencies, this.file, call);
+  }
 };
 
 if (process.env.NODE_ENV === 'test') {
-    Sassaby.setVariables = setVariables;
-    Sassaby.setDependencies = setDependencies;
+  Sassaby.setVariables = setVariables;
+  Sassaby.setDependencies = setDependencies;
 }
 
 module.exports = Sassaby;
